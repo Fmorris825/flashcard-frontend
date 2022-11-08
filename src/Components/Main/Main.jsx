0ee9dc1aae19039fd55901ac35d452cb55f2173b
Fred Morris <FredrickCPMorris@hotmail.com>
2022-11-07 18:19:45 -0600

    Added useEffect method to update collections on page render.

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import Sidebar from "../Sidebar.jsx/Sidebar";
 
 const Main = () => {
+  useEffect(() => {
+    getAllCollections();
+  }, []);
+
   const [collections, setCollections] = useState([]);
   const [card, setCard] = useState([]);
 
@@ -11,6 +15,7 @@ const Main = () => {
     const response = await axios.get("http://127.0.0.1:8000/api/collections/");
     setCollections(response.data);
   }
+  console.log(collections);
 
   return (
     <div>
