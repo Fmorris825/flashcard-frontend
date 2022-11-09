30f2a16f108fef100c72186953d4abbbcfef614c
Fred Morris <FredrickCPMorris@hotmail.com>
2022-11-09 10:24:38 -0600

    Created NextBtn and PreviousBtn components.

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import AddCollectionBtn from "../../AddCollectionBtn/AddCollectionBtn";
-import AddCardBtn from "../../AddCardBtn/AddCardBtn";
+import AddCollectionBtn from "../AddCollectionBtn/AddCollectionBtn";
+import AddCardBtn from "../AddCardBtn/AddCardBtn";
 import CardViewer from "../CardViewer/CardViewer";
 
 const CardContainer = ({ activeCollectionId }) => {
